1e21bffab25109ab3aa92f5d46d3f42a1a0c67a9
Huy Trương <dragon1998qn@gmail.com>
2021-11-26 13:51:09 +0700

    Done level 4 - Salting and Hashing passwords with bcrypt

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+// const encrypt = require("mongoose-encryption");
 const md5 = require("md5");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = new express();
 
@@ -21,7 +23,7 @@ const userScheme = new mongoose.Schema({
     password: String,
 });
 
-userScheme.plugin(encrypt, {secret: process.env.DB_SECRET_KEY, encryptedFields: ["password"]});
+// userScheme.plugin(encrypt, {secret: process.env.DB_SECRET_KEY, encryptedFields: ["password"]});
 
 const User = mongoose.model("users", userScheme);
 
@@ -37,10 +39,12 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", async (req, res) => {
-    const newUser = new User({
+app.post("/register",  (req, res) => {
+
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+        const newUser = new User({
         username: req.body.username,
-        password: md5(req.body.password),
+        password: hash,
     });
     try {
         await newUser.save();
@@ -48,16 +52,21 @@ app.post("/register", async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+    });
+
+    
 });
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     try {
         const foundUser = await User.findOne({ username: username });
-        const checkPass = await (foundUser.password === password);
+        const checkPass = await bcrypt.compare(password, foundUser.password);
         if (checkPass === true) {
             res.render("secrets");
+        } else {
+            res.redirect('/login');
         }
     } catch (error) {
         console.log(error);
