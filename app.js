8bc3a6e1811213d27721a2861dbca77206b2317e
Huy Trương <dragon1998qn@gmail.com>
2021-11-28 07:20:04 +0700

    Done level 6 - OAuth 2.0

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = new express();
 
@@ -34,19 +36,58 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userScheme = new mongoose.Schema({
     username: String,
     password: String,
+    googleId: String,
 });
 
 userScheme.plugin(passportLocalMongoose);
+userScheme.plugin(findOrCreate);
+
 // userScheme.plugin(encrypt, {secret: process.env.DB_SECRET_KEY, encryptedFields: ["password"]});
 const User = mongoose.model("users", userScheme);
+
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            User.findOrCreate({ googleId: profile.id }, function (err, user, createdUser) {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        res.redirect("/secrets");
+    }
+);
 
 app.get("/login", (req, res) => {
     res.render("login");
@@ -64,9 +105,9 @@ app.get("/secrets", (req, res) => {
     }
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
 });
 
 app.post("/register", async (req, res) => {
